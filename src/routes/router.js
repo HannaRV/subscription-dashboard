4a00b6561edec306ce36f3e2d94503ffc92fe7d3
Hanna Vretby <hr222sy@student.lnu.se>
2025-10-08 13:11:45 +0200

    Add minimal router.js for testing

---
+++ src/routes/router.js
@@ -0,0 +1,13 @@
+/**
+ * @file Main application router.
+ * @module src/routes/router.js
+ * @author Hanna Rubio Vretby <hr222sy@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import express from 'express'
+
+export const router = express.Router()
+router.get('/', (req, res) => {
+    res.send('<h1>Subscription Dashboard</h1><p>App is running!</p>')
+})
