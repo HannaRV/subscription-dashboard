f61de9f45d0c586bac288749fb4c13d77f2d1ac3
Hanna Vretby <hr222sy@student.lnu.se>
2025-10-08 13:02:04 +0200

    Add server.js Previous: Add config/express.js

--- src/server.js
+++ src/server.js
@@ -4,3 +4,13 @@
  * @author Hanna Rubio Vretby <hr222sy@student.lnu.se>
  * @version 1.0.0
  */
+
+import { ApplicationServer } from './config/express.js'
+
+const PORT = process.env.PORT || 3000
+const server = new ApplicationServer()
+const app = server.getApplication()
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
