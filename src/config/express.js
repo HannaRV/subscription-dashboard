6ad20a82b2e558c9a9bba34c876e63dd803d4cd8
Hanna Vretby <hr222sy@student.lnu.se>
2025-10-11 16:53:15 +0200

    Add js doc to express.js

--- src/config/express.js
+++ src/config/express.js
@@ -8,9 +8,15 @@
 import express from 'express'
 import { SubscriptionRouter } from '../routes/SubscriptionRouter.js'
 
+/**
+ * Represents the web server with Express configuration.
+ */
 export class WebServer {
     #expressApplication
 
+    /**
+     * Creates a new web server instance with configured Express.
+     */
     constructor() {
         this.#expressApplication = express()
         this.#configureMiddleware()
@@ -18,20 +24,33 @@ export class WebServer {
         this.#configureRoutes()
     }
 
+    /**
+     * Configures Express middleware for parsing requests.
+     */
     #configureMiddleware() {
         this.#expressApplication.use(express.json())
         this.#expressApplication.use(express.urlencoded({ extended: true }))
     }
-
+    /**
+     * Configures static file serving from public directory.
+     */
     #configureStaticFiles() {
         this.#expressApplication.use(express.static('src/public'))
     }
 
+    /**
+     * Configures application routes.
+     */
     #configureRoutes() {
         const subscriptionRouter = new SubscriptionRouter()
         this.#expressApplication.use('/', subscriptionRouter.getRouter())
     }
 
+    /**
+     * Returns the configured Express application instance.
+     * 
+     * @returns {express.Application} Express application instance
+     */
     getApplication() {
         return this.#expressApplication
     }
