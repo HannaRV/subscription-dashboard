a5a2405b76658198bcd96fe384552454de32b9d8
Hanna Vretby <hr222sy@student.lnu.se>
2025-10-12 16:48:54 +0200

    Add SubscriptionElementFactory class to main.js

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -11,10 +11,10 @@ import { API, CSS_CLASSES, DOM_IDS, MESSAGES } from './config.js';
  * Handles API communication for subscriptions.
  */
 class SubscriptionAPI {
-    #baseUrl;
+    #baseUrl
 
     constructor(baseUrl = API.BASE_URL) {
-        this.#baseUrl = baseUrl;
+        this.#baseUrl = baseUrl
     }
 
     /**
@@ -34,10 +34,35 @@ class SubscriptionAPI {
             return await response.json()
         } catch (error) {
             if (error.name === 'TypeError') {
-                throw new Error(MESSAGES.NETWORK_ERROR);
+                throw new Error(MESSAGES.NETWORK_ERROR)
             }
-            throw error;
+            throw error
         }
     }
 }
 
+/**
+ * Factory for creating subscription DOM elements.
+ */
+class SubscriptionElementFactory {
+    /**
+        * Creates a complete subscription element.
+        * 
+        * @param {Object} subscription - Subscription data
+        * @returns {HTMLElement} Subscription element
+        */
+    static createSubscriptionElement(subscription) {
+        const div = document.createElement('div')
+        div.className = CSS_CLASSES.SUBSCRIPTION_ITEM
+
+        const info = this.createInfoElement(subscription)
+        const price = this.createPriceElement(subscription.price)
+        const removeBtn = this.createRemoveButton(subscription.name)
+
+        div.appendChild(info)
+        div.appendChild(price)
+        div.appendChild(removeBtn)
+
+        return div
+    }
+}
