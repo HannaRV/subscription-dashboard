198d219d9147f522c4e6dfab58ddf9e07560b655
Hanna Vretby <hr222sy@student.lnu.se>
2025-10-13 19:41:54 +0200

    Add HTTP status code constants

---
+++ src/config/httpStatus.js
@@ -0,0 +1,13 @@
+/**
+ * @file HTTP status code constants shared across middlewares.
+ * Only includes status codes used in the application.
+ * @module src/config/httpStatus.js
+ * @author Hanna Rubio Vretby <hr222sy@student.lnu.se>
+ * @version 1.0.0
+ */
+export const HTTP_STATUS = {
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  TOO_MANY_REQUESTS: 429,
+  INTERNAL_SERVER_ERROR: 500
+}
