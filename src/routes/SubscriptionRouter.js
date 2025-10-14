2b2e033020638811335f6af4aa47734d9a9ff57a
Hanna Vretby <hr222sy@student.lnu.se>
2025-10-14 17:09:04 +0200

    Update SubscriptionRouter

--- src/routes/SubscriptionRouter.js
+++ src/routes/SubscriptionRouter.js
@@ -7,6 +7,7 @@
 
 import express from 'express'
 import { SubscriptionController } from '../controllers/SubscriptionController.js'
+import { SubscriptionValidation } from '../middleware/SubscriptionValidation.js'
 
 /**
  * Manages routes for subscription operations.
@@ -29,20 +30,22 @@ export class SubscriptionRouter {
      */
     #configureRoutes() {
         // Serve HTML dashboard
-        this.#router.get('/', (req, res) => 
+        this.#router.get('/', (req, res) =>
             this.#subscriptionController.displayDashboard(req, res))
 
         // API endpoint for subscription data
-        this.#router.get('/api/subscriptions', (req, res) => 
+        this.#router.get('/api/subscriptions', (req, res) =>
             this.#subscriptionController.getSubscriptionsData(req, res))
 
         // Add subscription
-        this.#router.post('/add', (req, res) => 
-            this.#subscriptionController.addSubscription(req, res))
+        this.#router.post('/add',
+            SubscriptionValidation.validateNewSubscription,
+            (req, res) => this.#subscriptionController.addSubscription(req, res))
 
         // Remove subscription by name
-        this.#router.post('/remove/:name', (req, res) => 
-            this.#subscriptionController.removeSubscription(req, res))
+        this.#router.post('/remove/:name',
+            SubscriptionValidation.validateSubscriptionName,
+            (req, res) => this.#subscriptionController.removeSubscription(req, res))
     }
 
     /**
